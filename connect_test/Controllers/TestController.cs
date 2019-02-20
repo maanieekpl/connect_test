e08ef7b90a1fdc351b3d4ff7e9dd4b577479d8cd
Adrian K <adrian_koszowski@outlook.com>
2019-02-20 11:44:35 +0100

    Add project files.

---
+++ connect_test/Controllers/TestController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using connect_test.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace connect_test.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly MyDbContext _context;
+
+        public TestController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        //GET: Tests
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Test.ToListAsync());
+        }
+
+        //GET: Test/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: Test/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Type")] Test test)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(test);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(test);
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
---
+++ connect_test/Models/MyDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace connect_test.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+
+        public DbSet<Test> Test { get; set; }
+               
+    }
+
+    public class DataSettingManager
+    {
+        private const string _dataSettingsFilePath = "App_Data/dataSetting.json";
+        public virtual DataSettings LoadSettings()
+        {
+            var text = File.ReadAllText(_dataSettingsFilePath);
+            if (string.IsNullOrEmpty(text))
+                return new DataSettings();
+
+            //get data settings from the JSON file
+            DataSettings settings = JsonConvert.DeserializeObject<DataSettings>(text);
+                return settings;
+        }
+    }
+
+    public class DataSettings
+    {
+        public string DataConnectionString { get; set; }
+    }
+}
